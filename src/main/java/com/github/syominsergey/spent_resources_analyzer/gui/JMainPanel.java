9cf9a8a66446f429421ec0113806749a978a9cf9
Sergei Semin <syominsergey@gmail.com>
2017-06-26 20:50:38 +0300

    добавлена кнопка выбора файла и кнопка учёта общего времени

--- src/main/java/com/github/syominsergey/spent_resources_analyzer/gui/JMainPanel.java
+++ src/main/java/com/github/syominsergey/spent_resources_analyzer/gui/JMainPanel.java
@@ -1,7 +1,6 @@
 package com.github.syominsergey.spent_resources_analyzer.gui;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 
 /**
@@ -9,10 +8,13 @@ import java.awt.*;
  */
 public class JMainPanel extends JEnableRecursivePanel {
 
+    JButton chooseButton = new JButton("Выбрать");
     JTextField fileNameTextField = new JTextField(20);
     JButton loadButton = new JButton("Загрузить");
     JComboBox functionComboBox = new JComboBox();
     JComboBox categoryNameComboBox = new JComboBox();
+    JCheckBox showRelativeToTotalTime = new JCheckBox("Показывать в долях к общему времени: ");
+    JTextField totalTimeTextField = new JTextField();
     JPanel diagramPanel = new JPanel();
     JCheckBox isFilterEnabledCheckBox = new JCheckBox("Разрешить фильтр", false);
     JFilterPanel filterPanel = new JFilterPanel();
@@ -22,14 +24,17 @@ public class JMainPanel extends JEnableRecursivePanel {
         JPanel northPanel = new JPanel(new BorderLayout());
         JPanel fileNamePanel = new JPanel();
         fileNamePanel.add(new JLabel("Путь к файлу"));
+        fileNamePanel.add(chooseButton);
         fileNamePanel.add(fileNameTextField);
         fileNamePanel.add(loadButton);
         northPanel.add(fileNamePanel, BorderLayout.NORTH);
-        JPanel analyzeSettingsPanel = new JPanel(new GridLayout(2, 2));
+        JPanel analyzeSettingsPanel = new JPanel(new GridLayout(3, 2));
         analyzeSettingsPanel.add(new JLabel("Анализируемая функция: "));
         analyzeSettingsPanel.add(functionComboBox);
         analyzeSettingsPanel.add(new JLabel("Анализ в категории: "));
         analyzeSettingsPanel.add(categoryNameComboBox);
+        analyzeSettingsPanel.add(showRelativeToTotalTime);
+        analyzeSettingsPanel.add(totalTimeTextField);
         northPanel.add(analyzeSettingsPanel, BorderLayout.CENTER);
         add(northPanel, BorderLayout.NORTH);
         diagramPanel.setPreferredSize(new Dimension(200, 200));
